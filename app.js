1ae73cbeb6cd8a9a1b566e75f44349f20108f8f9
Kshitij Thatte <62874526+kshitijthatte@users.noreply.github.com>
2021-12-17 10:22:35 +0530

    updated class timing

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 const countdown = () => {
-    const countDate = new Date("Jan 02, 2022 00:00:00").getTime();
+    const countDate = new Date("Jan 02, 2022 18:00:00").getTime();
     const now = new Date().getTime();
     const gap = countDate - now;
   
